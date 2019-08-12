4f8e1039cab231efcc4423d60c492052a023d42f
Oscar <ocuadrosl@gmail.com>
2019-08-12 13:06:03 -0300

    palette and line color red

--- drawingarea.cpp
+++ drawingarea.cpp
@@ -1,11 +1,19 @@
 #include "drawingarea.h"
 #include "ui_drawingarea.h"
+#include <QPalette>
 
 DrawingArea::DrawingArea(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::DrawingArea)
 {
     ui->setupUi(this);
+    QPalette pal = palette();
+
+    // set white background
+    pal.setColor(QPalette::Background, Qt::white);
+    this->setAutoFillBackground(true);
+    this->setPalette(pal);
+    this->show();
 
 
 
@@ -28,7 +36,7 @@ void DrawingArea::paintEvent(QPaintEvent *)
     if(qPoints.empty())
         return;
 
-
+    painter.setPen(Qt::red);
     for(qPointsT::iterator it = qPoints.begin(); it != qPoints.end()-1; ++it)
     {
         painter.drawLine(*it, *(it+1));
